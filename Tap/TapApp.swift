0f80232a1f46f374a7e2315e626e5a4a0a432725
Yen Hung <yen4meter@gmail.com>
2022-07-10 22:24:45 +0800

    Initial Commit

---
+++ Tap/TapApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapApp.swift
+//  Tap
+//
+//  Created by user on 2022/7/10.
+//
+
+import SwiftUI
+
+@main
+struct TapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
